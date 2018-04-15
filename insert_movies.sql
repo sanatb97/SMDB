6006a7c4e40c0a7ff949e6d4e677d2332705560e
Sanat Bhandarkar <31770675+sanatb97@users.noreply.github.com>
2018-04-15 12:36:21 +0530

    Add files via upload

---
+++ insert_movies.sql
@@ -0,0 +1,170 @@
+\c smdb
+INSERT INTO movie values('inc01','Inception',140,2010,'16/06/2010',825000000);
+INSERT INTO movie values('shr02','The Shawshank Redemption',142,1994,'17/02/1994',58000000);
+INSERT INTO movie values('dkr03','The Dark Knight',152,2008,'18/07/2008',534000000);
+INSERT INTO movie values('dkr04','The Dark Knight Rises',164,2012,'10/07/2012',1000000000);
+INSERT INTO movie values('shi05','Shutter Island',138,2010,'12/03/2010',294000000);
+INSERT INTO movie values('rhr06','Rush Hour 1',98,1998,'18/09/1998',141000000);
+INSERT INTO movie values('rhr07','Rush Hour 2',90,2001,'03/08/2001',347000000);
+INSERT INTO movie values('rhr08','Rush Hour 3',91,2007,'10/08/2007',140000000);
+INSERT INTO movie values('blp09','Black Panther',134,2018,'16/02/2018',639000000);
+INSERT INTO movie values('thr10','Thor Ragnarok',130,2017,'03/11/2017',315000000);
+INSERT INTO movie values('avg01','The Avengers',143,2012,'26/04/2012',623000000);
+INSERT INTO movie values('jtl01','Justice League',120,2017,'17/11/2017',229000000);
+INSERT INTO movie values('plf01','Pulp Fiction',154,1994,'21/10/2994',107000000);
+INSERT INTO movie values('ttx01','The Tuxedo',98,2002,'10/01/2002',50000000);
+INSERT INTO movie values('dan01','Dangal',161,2016,'21/12/2016',12000000);
+INSERT INTO movie values('tzp01','Taare Zameen Par',165,2007,'21/12/2007',1000000);
+INSERT INTO movie values('bjm01','Bajirao Mastani',158,2015,'18/12/2015',6000000);
+INSERT INTO movie values('bmb01','Bhaag Milkha Bhaag',186,2013,'12/07/2013',1000000);
+INSERT INTO movie values('udp01','Udta Punjab',148,2016,'17/06/2016',13000000);
+INSERT INTO movie values('pmv01','Padmaavat',164,2018,'25/01/2018',11000000);
+INSERT INTO movie values('bhg01','Baaghi',133,2016,'29/04/2016',18000000);
+INSERT INTO movie values('end01','Enthiran',174,2010,'01/10/2010',54000000);
+INSERT INTO movie values('kab01','Kabali',153,2016,'22/07/2016',46000000);
+INSERT INTO movie values('shb01','Shivaji The Boss',188,2007,'15/06/2007',19000000);
+INSERT INTO movie values('ron01','Rock On!!',148,2008,'29/07/2008',7000000);
+INSERT INTO movie values('jum01','Jumanji',104,1995,'16/02/1995',262000000);
+INSERT INTO movie values('slm01','Slumdog Mullionaire',120,2008,'25/12/2008',378000000);
+INSERT INTO movie values('tmi01','The Man Who Knew Infinity',108,2016,'08/04/2016',12000000);
+
+
+
+
+INSERT into actor values('leo01','Leonardo','DiCaprio','Male','11/11/74','');
+INSERT into actor values('jgl02','Joseph','Gordon-Levitt','Male','17/02/81','');
+INSERT into actor values('mof03','Morgan','Freeman','Male','01/06/37','');
+INSERT into actor values('tir04','Tim','Robbins','Male','16/10/58','');
+INSERT into actor values('chb05','Christian','Bale','Male','13/01/74','');
+INSERT into actor values('hel06','Heath','Ledger','Male','04/04/79','');
+INSERT into actor values('toh07','Tom','Hardy','Male','15/09/77','');
+INSERT into actor values('anh08','Anne','Hathway','Female','12/11/82','');
+INSERT into actor values('dep09','Dev' ,'Patel','Male','23/04/90','');
+INSERT into actor values('mar10','Mark','Ruffalo','Male','22/11/67','');
+INSERT into actor values('bek11','Ben','Kingsley','Male','31/12/43','');
+INSERT into actor values('rdj12','Robert','Downey Jr.','Male','04/04/65','');
+INSERT into actor values('che13','Chris','Evans','Male','13/04/81','');
+INSERT into actor values('jac13','Jackie','Chan','Male','07/04/54','');
+INSERT into actor values('cht14','Chris','Tucker','Male','31/08/71','');
+INSERT into actor values('scj15','Scarlett','Johansson','Female','22/11/84','');
+
+
+
+INSERT into director values('chn01','Christopher','Nolan','30/07/70');
+INSERT into director values('frd02','Frank','Darabont','28/01/59');
+INSERT into director values('brr04','Brett','Ratner','28/03/69');
+INSERT into director values('ryc05','Ryan','Coogler','23/06/86');
+INSERT into director values('taw06','Taika','Waititi','16/08/75');
+INSERT into director values('jow07','Joss','Whedon','23/06/64');
+INSERT into director values('zas08','Zack','Snyder','01/03/66');
+INSERT into director values('qut09','Quentin','Tarantino','27/03/63');
+INSERT into director values('ked10','Kevin','Donovan','17/12/71');
+INSERT into director values('mas03','Martin','Scorsese','17/11/42');
+
+
+
+INSERT into genre values('gen01','Action');
+INSERT into genre values('gen02','Adventure');
+INSERT into genre values('gen03','Animation');
+INSERT into genre values('gen04','Biography');
+INSERT into genre values('gen05','Comedy');
+INSERT into genre values('gen06','Crime');
+INSERT into genre values('gen08','Drama');
+INSERT into genre values('gen09','Family');
+INSERT into genre values('gen10','Fantasy');
+INSERT into genre values('gen11','History');
+INSERT into genre values('gen12','Horror');
+INSERT into genre values('gen13','Musical');
+INSERT into genre values('gen14','Mystery');
+INSERT into genre values('gen15','Romance');
+INSERT into genre values('gen16','Sci-Fi');
+INSERT into genre values('gen17','Short');
+INSERT into genre values('gen18','Sport');
+INSERT into genre values('gen19','Superhero');
+INSERT into genre values('gen20','Thriller');
+INSERT into genre values('gen21','War');
+INSERT into genre values('gen22','Western');
+
+
+
+INSERT into writer values('chn01','Christopher Nolan','inc01');
+INSERT into writer values('frd02','Frank Darabont','shr02');
+INSERT into writer values('chn02','Christopher Nolan','dkr03');
+INSERT into writer values('chn03','Christopher Nolan','dkr04');
+INSERT into writer values('lak03','Laeta Kalogridis','shi05');
+INSERT into writer values('rol04','Ross LaManna','rhr06');
+INSERT into writer values('jko05','Jim Kouf','rhr07');
+INSERT into writer values('jko06','Jim Kouf','rhr08');
+INSERT into writer values('ryc05','Ryan Coogler','blp09');
+INSERT into writer values('epe06','Eric Pearson','thr10');
+
+
+
+INSERT into songs values('incs1','Half Remembered Dream','inc01','1:12');
+INSERT into songs values('incs2','We Built Our Own World','inc01','1:55');
+INSERT into songs values('incs3','Dream Is Collapsing','inc01','2:28');
+INSERT into songs values('incs4','Radical Notion','inc01','3:43');
+INSERT into songs values('incs5','Old Souls','inc01','7:44');
+INSERT into songs values('shrs1','May','shr02','0:33');
+INSERT into songs values('shrs2','Shawshank Prison','shr02','1:53');
+INSERT into songs values('shrs3','New Fish','shr02','1:50');
+INSERT into songs values('shrs4','Rock Hammer','shr02','1:51');
+INSERT into songs values('shrs5','An Inch of His Life','shr02','2:48');
+
+
+INSERT INTO music_director VALUES ('haz01','Hans Zimmer','inc01');
+INSERT INTO music_director VALUES ('thn02','Thomas Newman','shr02');
+INSERT INTO music_director VALUES ('haz03','Hans Zimmer','dkr03');
+INSERT INTO music_director VALUES ('haz04','Hans Zimmer','dkr04');
+INSERT INTO music_director VALUES ('ror05','Robbie Robertson','shi05');
+INSERT INTO music_director VALUES ('las06','Lalo Schifrin','rhr06');
+INSERT INTO music_director VALUES ('las07','Lalo Schifrin','rhr07');
+INSERT INTO music_director VALUES ('bob08','Bob Bowen','rhr08');
+INSERT INTO music_director VALUES ('lug09','Ludwig Göransson','blp09');
+INSERT INTO music_director VALUES ('mam10','Mark Mothersbaugh','thr10');
+
+
+INSERT INTO acts_in values('inc01','leo01');
+INSERT INTO acts_in values('inc01','jgl02');
+INSERT INTO acts_in values('dkr03','chb05');
+INSERT INTO acts_in values('dkr04','chb05');
+INSERT INTO acts_in values('dkr04','hel06');
+INSERT INTO acts_in values('rhr06','jac13');
+INSERT INTO acts_in values('rhr07','jac13');
+INSERT INTO acts_in values('rhr08','jac13');
+INSERT INTO acts_in values('dkr04','anh08');
+INSERT INTO acts_in values('rhr06','cht14');
+INSERT INTO acts_in values('rhr07','cht14');
+INSERT INTO acts_in values('shr02','mof03');
+INSERT INTO acts_in values('shr02','tir04');
+
+
+
+
+INSERT INTO movie_genres values('inc01','gen01');
+INSERT INTO movie_genres values('shr02','gen14');
+INSERT INTO movie_genres values('dkr03','gen01');
+INSERT INTO movie_genres values('dkr04','gen01');
+INSERT INTO movie_genres values('shi05','gen14');
+INSERT INTO movie_genres values('rhr06','gen05');
+INSERT INTO movie_genres values('rhr07','gen05');
+INSERT INTO movie_genres values('rhr08','gen05');
+INSERT INTO movie_genres values('blp09','gen19');
+INSERT INTO movie_genres values('thr10','gen10');
+INSERT INTO movie_genres values('avg01','gen16');
+INSERT INTO movie_genres values('jtl01','gen19');
+INSERT INTO movie_genres values('plf01','gen05');
+
+INSERT INTO directs values('chn01','dkr03');
+INSERT INTO directs values('chn01','dkr04');
+INSERT INTO directs values('chn01','inc01');
+INSERT INTO directs values('frd02','shr02');
+INSERT INTO directs values('mas03','shi05');
+INSERT INTO directs values('brr04','rhr06');
+INSERT INTO directs values('brr04','rhr07');
+INSERT INTO directs values('brr04','rhr08');
+INSERT INTO directs values('ryc05','blp09');
+INSERT INTO directs values('taw06','thr10');
+INSERT INTO directs values('jow07','avg01');
+INSERT INTO directs values('zas08','jtl01');
+INSERT INTO directs values('qut09','plf01');
